8aec0580143d9e37fde2b203bda5d537bc2767a1
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-04 19:27:19 -0400

    Add files via upload

---
+++ formatt.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+echo -e "\nInstalling prereqs...\n$HR"
+pacman -S --noconfirm gptfdisk btrfs-progs
+
+echo "-------------------------------------------------"
+echo "-------select your disk to format----------------"
+echo "-------------------------------------------------"
+lsblk
+echo "Please enter disk: (example /dev/sda)"
+read DISK
+echo "--------------------------------------"
+echo -e "\nFormatting disk...\n$HR"
+echo "--------------------------------------"
+
+# disk prep
+sgdisk -Z ${DISK} # zap all on disk
+sgdisk -a ${DISK} # new gpt disk 2048 alignment
+
+
+# create partitions
+sgdisk -n 1:0:+5000M ${DISK} # partition 1 (UEFI SYS), default start block, 5GB
+sgdisk -n 2:0:+195000M ${DISK} # partition 2 (Root), default start block, 195GB
+
+# set partition types
+sgdisk -t 1:ef00 ${DISK}
+sgdisk -t 2:8300 ${DISK}
+
+# label partitions
+sgdisk -c 1:"UEFISYS" ${DISK}
+sgdisk -c 2:"ROOT" ${DISK}
+
+# make filesystems
+echo -e "\nCreating Filesystems...\n$HR"
+
+mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
+mkfs.ext4 -L "ROOT" "${DISK}2"
+
+# mount target
+mkdir /mnt
+mount -t ext4 "${DISK}2" /mnt
+mkdir /mnt/boot
+mkdir /mnt/boot/efi
+mount -t vfat "${DISK}1" /mnt/boot/
+
+
+echo "--------------------------------------"
+echo "-- Arch Install on Main Drive       --"
+echo "--------------------------------------"
+pacstrap /mnt base base-devel linux linux-firmware git nano sudo --noconfirm --needed
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+
+arch-chroot /mnt
+
+git clone https://github.com/dspates81/ArchInst.git
+
+#./ArchInst/Arch_Installation.sh
