521230656563c6167d9e919234d8fcf27dd163a3
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-08 14:18:57 -0400

    Update pacstrap.sh

--- pacstrap.sh
+++ pacstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pacstrap /mnt base linux linux-firmware nano sudo git
+pacstrap /mnt base linux linux-firmware nano sudo git man
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 git clone https://github.com/dspates81/ArchInst
