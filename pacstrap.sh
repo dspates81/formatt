6185a646b08ce5e24989d4992c6d6184a341438d
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-14 19:24:04 -0400

    Update pacstrap.sh

--- pacstrap.sh
+++ pacstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-pacstrap -i /mnt base linux openssh sudo git man nano
+pacstrap -i /mnt base linux linux-firmware base-devel linux-headers openssh sudo git man nano
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
